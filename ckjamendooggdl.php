69044015469bacb0a4cb47632cd246e1bb7dff4d
Cyber Killer <cyber_killer@jabster.pl>
2011-07-27 14:06:03 +0200

    year in the license text updated
    --HG--
    extra : convert_revision : cyber_killer%40jabster.pl-20110727120603-u6rta83ibme6rhok

--- ckjamendooggdl.php
+++ ckjamendooggdl.php
@@ -4,7 +4,7 @@
 
 /*
 	CkJamendoOggDl - downloads albums from jamendo.com in OGG Vorbis format
-	Copyright (C) 2010 Łukasz "Cyber Killer" Korpalski
+	Copyright (C) 2011 Łukasz "Cyber Killer" Korpalski
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
